858ed1b122f4c2e54b9117b3028e27d4b589495b
pichaiyoot <pichaiyoot110@gmail.com>
2018-11-19 16:35:35 +0700

    Lab 1 commit

--- src/lab1/intro.java
+++ src/lab1/intro.java
@@ -1,4 +1,26 @@
 package lab1;
 
 public class intro {
-}
+
+    public static void main(String[] args) {
+
+        System.out.println("pichaiyoot Fuangnawakit");
+        System.out.println("pichaichaiyoot110@gmail.com");
+        System.out.println("information system");
+        System.out.println("RMUTSV");
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+
+
+
+}//class
